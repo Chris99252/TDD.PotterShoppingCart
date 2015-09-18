90c9a3e17f4535eea521afc955f157dba735bdc5
Chris99252 <chris@ccf.org.tw>
2015-09-18 11:05:28 +0800

    Inital commit

---
+++ PotterShoppingCart.Tests.Chris/PotterShoppingCartTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PotterShoppingCart.Tests.Chris
+{
+    [TestClass]
+    public class PotterShoppingCartTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
