eb6db7c0fd2ab2a161ceef45cd42a22515e1ced5
Chris99252 <chris@ccf.org.tw>
2015-09-19 00:09:51 +0800

    #6 commit 完成全部 test case 與商業邏輯

--- PotterShoppingCart.Service.Chris/PotterShopping.cs
+++ PotterShoppingCart.Service.Chris/PotterShopping.cs
@@ -14,7 +14,8 @@ namespace PotterShoppingCart.Service.Chris
             double total = 0;
             int kindCount = 0;
 
-            for (int i = 1; i < potterBooks.Count; i++)
+            // 取得購買的種類數
+            for (int i = 1; i <= potterBooks.Count; i++)
             {
                 var key = "episode" + i;
                 if (potterBooks[key] > 0)
@@ -23,19 +24,273 @@ namespace PotterShoppingCart.Service.Chris
                 }
             }
 
-            var bookCount = potterBooks.Sum(x => x.Value);
+            // 購買的總數量
+            var bookCount = potterBooks.Where(x => x.Value > 0).Sum(x => x.Value);
 
-            if (bookCount == 1 || kindCount == 1)
+            var data = potterBooks.Where(x => x.Value > 0).OrderBy(x => x.Value);
+
+            // 只買一個種類，不打折
+            if (kindCount == 1)
             {
                 total = bookCount * 100;
             }
-            else if (bookCount == 2)
+
+            // 買兩個種類，一組打九五折
+            else if (kindCount == 2)
             {
-                total = bookCount * 100 * 0.95;
+                total = data.ElementAt(0).Value * 2 * 100 * 0.95;
+                int kindCount2 = 0;
+                for (int i = 0; i < data.Count(); i++)
+                {
+                    if ((data.ElementAt(i).Value - data.ElementAt(0).Value) > 0)
+                    {
+                        kindCount2 += 1;
+                    }
+                }
+                if (kindCount2 == 1)
+                {
+                    total = total + (data.ElementAt(1).Value - data.ElementAt(0).Value) * 100;
+                }
             }
-            else if (bookCount == 3)
+            // 買三個種類，一組打九折
+            else if (kindCount == 3)
             {
-                total = bookCount * 100 * 0.9;
+                total = data.ElementAt(0).Value * 3 * 100 * 0.9;
+
+                int kindCount2 = 0;
+                for (int i = 0; i < data.Count(); i++)
+                {
+                    if ((data.ElementAt(i).Value - data.ElementAt(0).Value) > 0)
+                    {
+                        kindCount2 += 1;
+                    }
+                }
+
+                if (kindCount2 == 2)
+                {
+                    total = total + (data.ElementAt(1).Value - data.ElementAt(0).Value) * 2 * 100 * 0.95;
+                    int kindCount3 = 0;
+                    for (int i = 0; i < data.Count(); i++)
+                    {
+                        if ((data.ElementAt(i).Value - data.ElementAt(1).Value) > 0)
+                        {
+                            kindCount3 += 1;
+                        }
+                    }
+                    if (kindCount3 == 1)
+                    {
+                        total = total + (data.ElementAt(2).Value - data.ElementAt(1).Value) * 100;
+                    }
+                }
+                else if (kindCount2 == 1)
+                {
+                    total = total + (data.ElementAt(2).Value - data.ElementAt(1).Value) * 100;
+                }
+            }
+            // 買四個種類，一組打八折
+            else if (kindCount == 4)
+            {
+                total = data.ElementAt(0).Value * 4 * 100 * 0.8;
+
+                int kindCount2 = 0;
+                for (int i = 0; i < data.Count(); i++)
+                {
+                    if ((data.ElementAt(i).Value - data.ElementAt(0).Value) > 0)
+                    {
+                        kindCount2 += 1;
+                    }
+                }
+
+                if (kindCount2 == 3)
+                {
+                    total = total + (data.ElementAt(1).Value - data.ElementAt(0).Value) * 3 * 100 * 0.9;
+
+                    int kindCount3 = 0;
+                    for (int i = 0; i < data.Count(); i++)
+                    {
+                        if ((data.ElementAt(i).Value - data.ElementAt(1).Value) > 0)
+                        {
+                            kindCount3 += 1;
+                        }
+                    }
+
+                    if (kindCount3 == 2)
+                    {
+                        total = total + (data.ElementAt(2).Value - data.ElementAt(1).Value) * 2 * 100 * 0.95;
+                        int kindCount4 = 0;
+                        for (int i = 0; i < data.Count(); i++)
+                        {
+                            if ((data.ElementAt(i).Value - data.ElementAt(2).Value) > 0)
+                            {
+                                kindCount4 += 1;
+                            }
+                        }
+                        if (kindCount4 == 1)
+                        {
+                            total = total + (data.ElementAt(3).Value - data.ElementAt(2).Value) * 100;
+                        }
+                    }
+                    else if (kindCount3 == 1)
+                    {
+                        total = total + (data.ElementAt(3).Value - data.ElementAt(2).Value) * 100;
+                    }
+                }
+                else if (kindCount2 == 2)
+                {
+                    total = total + (data.ElementAt(2).Value - data.ElementAt(1).Value) * 2 * 100 * 0.95;
+                    int kindCount3 = 0;
+                    for (int i = 0; i < data.Count(); i++)
+                    {
+                        if ((data.ElementAt(i).Value - data.ElementAt(2).Value) > 0)
+                        {
+                            kindCount3 += 1;
+                        }
+                    }
+                    if (kindCount3 == 1)
+                    {
+                        total = total + (data.ElementAt(3).Value - data.ElementAt(2).Value) * 100;
+                    }
+                }
+                else if (kindCount2 == 1)
+                {
+                    total = total + (data.ElementAt(3).Value - data.ElementAt(2).Value) * 100;
+                }
+            }
+            // 買五個種類，一組打七五折
+            else if (kindCount == 5)
+            {
+                total = data.ElementAt(0).Value * 5 * 100 * 0.75;
+
+                int kindCount2 = 0;
+                for (int i = 0; i < data.Count(); i++)
+                {
+                    if ((data.ElementAt(i).Value - data.ElementAt(0).Value) > 0)
+                    {
+                        kindCount2 += 1;
+                    }
+                }
+                if (kindCount2 == 4)
+                {
+                    total = (data.ElementAt(1).Value - data.ElementAt(0).Value) * 4 * 100 * 0.8;
+
+                    int kindCount3 = 0;
+                    for (int i = 0; i < data.Count(); i++)
+                    {
+                        if ((data.ElementAt(i).Value - data.ElementAt(1).Value) > 0)
+                        {
+                            kindCount3 += 1;
+                        }
+                    }
+
+                    if (kindCount3 == 3)
+                    {
+                        total = total + (data.ElementAt(2).Value - data.ElementAt(1).Value) * 3 * 100 * 0.9;
+
+                        int kindCount4 = 0;
+                        for (int i = 0; i < data.Count(); i++)
+                        {
+                            if ((data.ElementAt(i).Value - data.ElementAt(2).Value) > 0)
+                            {
+                                kindCount4 += 1;
+                            }
+                        }
+
+                        if (kindCount4 == 2)
+                        {
+                            total = total + (data.ElementAt(3).Value - data.ElementAt(2).Value) * 2 * 100 * 0.95;
+                            int kindCount5 = 0;
+                            for (int i = 0; i < data.Count(); i++)
+                            {
+                                if ((data.ElementAt(i).Value - data.ElementAt(3).Value) > 0)
+                                {
+                                    kindCount5 += 1;
+                                }
+                            }
+                            if (kindCount5 == 1)
+                            {
+                                total = total + (data.ElementAt(4).Value - data.ElementAt(3).Value) * 100;
+                            }
+                        }
+                        else if (kindCount4 == 1)
+                        {
+                            total = total + (data.ElementAt(4).Value - data.ElementAt(3).Value) * 100;
+                        }
+                    }
+                    else if (kindCount3 == 2)
+                    {
+                        total = total + (data.ElementAt(3).Value - data.ElementAt(2).Value) * 2 * 100 * 0.95;
+                        int kindCount5 = 0;
+                        for (int i = 0; i < data.Count(); i++)
+                        {
+                            if ((data.ElementAt(i).Value - data.ElementAt(3).Value) > 0)
+                            {
+                                kindCount5 += 1;
+                            }
+                        }
+                        if (kindCount5 == 1)
+                        {
+                            total = total + (data.ElementAt(4).Value - data.ElementAt(3).Value) * 100;
+                        }
+                    }
+                    else if (kindCount3 == 1)
+                    {
+                        total = total + (data.ElementAt(4).Value - data.ElementAt(3).Value) * 100;
+                    }
+                }
+                else if (kindCount2 == 3)
+                {
+                    total = total + (data.ElementAt(2).Value - data.ElementAt(1).Value) * 3 * 100 * 0.9;
+
+                    int kindCount3 = 0;
+                    for (int i = 0; i < data.Count(); i++)
+                    {
+                        if ((data.ElementAt(i).Value - data.ElementAt(2).Value) > 0)
+                        {
+                            kindCount3 += 1;
+                        }
+                    }
+
+                    if (kindCount3 == 2)
+                    {
+                        total = total + (data.ElementAt(3).Value - data.ElementAt(2).Value) * 2 * 100 * 0.95;
+                        int kindCount4 = 0;
+                        for (int i = 0; i < data.Count(); i++)
+                        {
+                            if ((data.ElementAt(i).Value - data.ElementAt(3).Value) > 0)
+                            {
+                                kindCount4 += 1;
+                            }
+                        }
+                        if (kindCount4 == 1)
+                        {
+                            total = total + (data.ElementAt(4).Value - data.ElementAt(3).Value) * 100;
+                        }
+                    }
+                    else if (kindCount3 == 1)
+                    {
+                        total = total + (data.ElementAt(4).Value - data.ElementAt(3).Value) * 100;
+                    }
+                }
+                else if (kindCount2 == 2)
+                {
+                    total = total + (data.ElementAt(3).Value - data.ElementAt(2).Value) * 2 * 100 * 0.95;
+                    int kindCount3 = 0;
+                    for (int i = 0; i < data.Count(); i++)
+                    {
+                        if ((data.ElementAt(i).Value - data.ElementAt(3).Value) > 0)
+                        {
+                            kindCount3 += 1;
+                        }
+                    }
+                    if (kindCount3 == 1)
+                    {
+                        total = total + (data.ElementAt(4).Value - data.ElementAt(3).Value) * 100;
+                    }
+                }
+                else if (kindCount2 == 1)
+                {
+                    total = total + (data.ElementAt(4).Value - data.ElementAt(3).Value) * 100;
+                }
             }
             return total;
         }
--- PotterShoppingCart.Tests.Chris/PotterShoppingCartTests.cs
+++ PotterShoppingCart.Tests.Chris/PotterShoppingCartTests.cs
@@ -13,7 +13,7 @@ namespace PotterShoppingCart.Tests.Chris
         /// Scenario: 第一集買了一本，其他都沒買，價格應為100*1=100元
         /// </summary>
         [TestMethod]
-        public void BuyOneBook_第一集買了一本其_他都沒買_價格應為100元()
+        public void 第一集買了一本其_他都沒買_價格應為100元()
         {
             // arrange
             var target = new PotterShopping();
@@ -38,19 +38,19 @@ namespace PotterShoppingCart.Tests.Chris
         /// Scenario: 第一集買了一本，第二集也買了一本，價格應為100*2*0.95=190
         /// </summary>
         [TestMethod]
-        public void BuyTwoBook_第一集買了一本_第二集也買了一本_價格應為_190元()
+        public void 第一集買了一本_第二集也買了一本_價格應為_190元()
         {
             // arrange
             var target = new PotterShopping();
 
             Dictionary<string, double> potterBooks = new Dictionary<string, double>();
-            potterBooks.Add("episode1", 1);
-            potterBooks.Add("episode2", 1);
+            potterBooks.Add("episode1", 3);
+            potterBooks.Add("episode2", 5);
             potterBooks.Add("episode3", 0);
             potterBooks.Add("episode4", 0);
             potterBooks.Add("episode5", 0);
 
-            double expected = 190;
+            double expected = 770;
 
             // act
             double actual = target.PotterCalculator(potterBooks);
@@ -60,7 +60,7 @@ namespace PotterShoppingCart.Tests.Chris
         }
 
         [TestMethod]
-        public void BuyTwoBook_第一集買了兩本_價格應為_200元()
+        public void 第一集買了兩本_價格應為_200元()
         {
             // arrange
             var target = new PotterShopping();
@@ -85,7 +85,7 @@ namespace PotterShoppingCart.Tests.Chris
         /// Scenario: 一二三集各買了一本，價格應為100*3*0.9=270
         /// </summary>
         [TestMethod]
-        public void BuyThreeBook_一二三集各買了一本_價格應為270元()
+        public void 一二三集各買了一本_價格應為270元()
         {
             // arrange
             var target = new PotterShopping();
@@ -98,6 +98,106 @@ namespace PotterShoppingCart.Tests.Chris
             potterBooks.Add("episode5", 0);
 
             double expected = 270;
+
+            // act
+            double actual = target.PotterCalculator(potterBooks);
+
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        /// <summary>
+        /// Scenario: 一二三四集各買了一本，價格應為100*4*0.8=320
+        /// </summary>
+        [TestMethod]
+        public void 一二三四集各買了一本_價格應為320元()
+        {
+            // arrange
+            var target = new PotterShopping();
+
+            Dictionary<string, double> potterBooks = new Dictionary<string, double>();
+            potterBooks.Add("episode1", 1);
+            potterBooks.Add("episode2", 1);
+            potterBooks.Add("episode3", 1);
+            potterBooks.Add("episode4", 1);
+            potterBooks.Add("episode5", 0);
+
+            double expected = 320;
+
+            // act
+            double actual = target.PotterCalculator(potterBooks);
+
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        /// <summary>
+        /// Scenario: 一次買了整套，一二三四五集各買了一本，價格應為375元
+        /// </summary>
+        [TestMethod]
+        public void 一二三四五集各買了一本_價格應為375元()
+        {
+            // arrange
+            var target = new PotterShopping();
+
+            Dictionary<string, double> potterBooks = new Dictionary<string, double>();
+            potterBooks.Add("episode1", 1);
+            potterBooks.Add("episode2", 1);
+            potterBooks.Add("episode3", 1);
+            potterBooks.Add("episode4", 1);
+            potterBooks.Add("episode5", 1);
+
+            double expected = 375;
+
+            // act
+            double actual = target.PotterCalculator(potterBooks);
+
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        /// <summary>
+        /// Scenario: 一二集各買了一本，第三集買了兩本，價格應為370元
+        /// </summary>
+        [TestMethod]
+        public void 一二集各買了一本_第三集買了兩本_價格應為370元()
+        {
+            // arrange
+            var target = new PotterShopping();
+
+            Dictionary<string, double> potterBooks = new Dictionary<string, double>();
+            potterBooks.Add("episode1", 1);
+            potterBooks.Add("episode2", 1);
+            potterBooks.Add("episode3", 2);
+            potterBooks.Add("episode4", 0);
+            potterBooks.Add("episode5", 0);
+
+            double expected = 370;
+
+            // act
+            double actual = target.PotterCalculator(potterBooks);
+
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        /// <summary>
+        /// Scenario: 第一集買了一本，第二三集各買了兩本，價格應為460元
+        /// </summary>
+        [TestMethod]
+        public void 第一集買了一本_第二三集各買了兩本_價格應為460元()
+        {
+            // arrange
+            var target = new PotterShopping();
+
+            Dictionary<string, double> potterBooks = new Dictionary<string, double>();
+            potterBooks.Add("episode1", 1);
+            potterBooks.Add("episode2", 2);
+            potterBooks.Add("episode3", 2);
+            potterBooks.Add("episode4", 0);
+            potterBooks.Add("episode5", 0);
+
+            double expected = 460;
 
             // act
             double actual = target.PotterCalculator(potterBooks);
